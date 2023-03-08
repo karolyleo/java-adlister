9e3f3d2ede31414a5eca176f2e16347e718cb27e
Leonard Karoly <leonard.a.karoly@gmail.com>
2023-03-08 16:48:38 -0600

    Playing with servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,12 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+        System.out.println("Hello World");
     }
 }
