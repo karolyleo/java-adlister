977f69e734c5b407207e734336bc7427246fd387
Leonard Karoly <leonard.a.karoly@gmail.com>
2023-03-10 16:00:59 -0600

    func broken down JSP

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    public int CostInCents;
+    public String name;
+
+    public Product(String name, int costInCents) {
+        CostInCents = costInCents;
+        this.name = name;
+    }
+}
